82dcf05a10b9451d6a20773e013b841e4d5cca8e
Abel Huanca <abel.huanca@upeu.pe>
2014-05-05 14:20:19 -0500

    agregando ads a admin

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from ads.models import Categoria, Elemento
 # Register your models here.
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    prepopulated_fields = {"slug": ("nombre",)}
+
+
+class ElementoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'categoria', 'seccion', 'publicado_en']
+    list_filter = ['categoria', 'publicado_en']
+
+
+admin.site.register(Elemento, ElementoAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
