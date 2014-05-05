4c640ea95d8e0ff582ffe8ec6f8441bffd594eca
Abel Huanca <abel.huanca@upeu.pe>
2014-05-05 14:03:15 -0500

    agregando modelo a ads

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+
+TIPO_ANUNCIO =(
+    ('C', 'Compra'),
+    ('V', 'Venta'),
+    ('A', 'Alquiler'),
+    ('T', 'Trabajo'),
+    ('O', 'Otros'),
+)
+
+
+class Elemento(models.Model):
+    anuncio = models.CharField(max_length=1, choices=TIPO_ANUNCIO, default='O')
+    nombre = models.CharField(max_length=255)
+    categoria = models.ForeignKey(Categoria)
+    seccion = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    publicado_en = models.DateTimeField(auto_now_add=True)
