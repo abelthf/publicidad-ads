6045fd57843f76b79198c14d171f8d17b1f2caa7
Alumno <alumno@debian.debian.fia.upeu.pe>
2014-05-12 13:34:36 -0500

    agregando vista index

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.template import Context
 
+from ads.models import Elemento
 # Create your views here.
+
+
+def index(request):
+    """ Construivr un metodo que sea la vista principal de la aplicacion
+    ads, el objetivo del metodo es mostrar todos los item de elementos."""
+
+    elementos = Elemento.objects.all()
+    context = Contex({'titulo': 'ADS site', 'elementos': elementos})
+    return render_to_response('ads/index.html', context,
+            context_instance=RequestContext(request))
+
+
+
--- publicidad/urls.py
+++ publicidad/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'publicidad.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^ads/', include('ads.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
